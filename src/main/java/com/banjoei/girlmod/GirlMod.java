5af3afab7fe3143b45449cf26b9b997ea4cfdd59
Banjoei <63517694+Banjoei@users.noreply.github.com>
2021-05-12 18:28:59 -0400

    First Item added

--- src/main/java/com/banjoei/girlmod/GirlMod.java
+++ src/main/java/com/banjoei/girlmod/GirlMod.java
@@ -1,10 +1,14 @@
 package com.banjoei.girlmod;
 
+import com.banjoei.girlmod.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class GirlMod implements ModInitializer {
+
+    public static final String MOD_ID = "girlmod_and_extras";
+
     @Override
     public void onInitialize() {
-
+        ModItems.registerItems();
     }
 }
