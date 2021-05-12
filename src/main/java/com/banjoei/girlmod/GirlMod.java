e502f190616a0ea299caa8a5a33deca0c93793a0
Banjoei <63517694+Banjoei@users.noreply.github.com>
2021-05-12 02:52:04 -0400

    Defining Mod (P2)

---
+++ src/main/java/com/banjoei/girlmod/GirlMod.java
@@ -0,0 +1,10 @@
+package com.banjoei.girlmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GirlMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
